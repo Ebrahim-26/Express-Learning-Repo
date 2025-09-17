627a2764aa77db6f66486535f1b66b9ba00b6094
ebrahim <ebuebrahim786@gmail.com>
2025-09-17 14:05:31 +0800

    Created server.js file where we used app.get() method to define the url and what we want to respond with,
    in this process we created html file to display the contents

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+app.listen(8000, () => console.log(`Server is running on server 8000`));
