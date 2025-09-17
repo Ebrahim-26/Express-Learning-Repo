0b2240b50c97cc8d47c30433ec58944a75d9a65d
ebrahim <ebuebrahim786@gmail.com>
2025-09-17 14:22:05 +0800

    Sent the posts data on hitting /api/posts

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
 //setup static folder
-app.use(express.static(path.join(__dirname,'public')))
+// app.use(express.static(path.join(__dirname, "public")));
 //app.use = middleware
-//express.static is how we set the static file. It takes in the path.
+//express.static is how we set the static file. It takes in the path./
+
+let posts = [
+  { id: 1, title: "post 1" },
+  { id: 2, title: "post 2" },
+  { id: 3, title: "post 3" },
+];
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
 
 app.listen(8000, () => console.log(`Server is running on server 8000`));
